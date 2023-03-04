a0722f321bba818fee27fc9357ec755b10742f2a
Keyan Tabor <keyantabor98@gmail.com>
2023-03-04 03:56:09 -0500

    added on click founders to home page/ etc

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import "./Home.css"
-import MovingFounder from './MovingFounder';
+import RandomFounder from './RandomFounder';
+
 class Home extends Component {
     render() {
         return (
             <div>
+
                 <h1>Welcome to the Home Page</h1>
                 <h3>Still Under Construction</h3>
-                <MovingFounder />
+                <br></br>
+                <RandomFounder />
             </div>
         )
     }
